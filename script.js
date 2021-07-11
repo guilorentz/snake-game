62b6e2968c1e6ea141e5748bcd927305a530f46c
guilorentz <go.lorentz@gmail.com>
2021-07-11 00:46:02 -0400

    definir aletoriedade da comida e tamanho da cobrinha

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ snake[0] = {
 };
 let direction = "right";
 let food = {
-	x: Math.floor(Math.random() * 15 + 1) * box,
-	y: Math.floor(Math.random() * 15 + 1) * box,
-}
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+};
 
 function criarBG() {
   context.fillStyle = "lightblue";
@@ -25,8 +25,8 @@ function criarCobrinha() {
 }
 
 function drawFood() {
-	context.fillStyle = "red";
-	context.fillRect(food.x, food.y, box, box);
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -46,7 +46,7 @@ function iniciarJogo() {
 
   criarBG();
   criarCobrinha();
-	drawFood();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -56,7 +56,12 @@ function iniciarJogo() {
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
 
-  snake.pop();
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
 
   let newHead = {
     x: snakeX,
